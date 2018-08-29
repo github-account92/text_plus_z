6676d63d7b2b8b2c275248b8bfe3fff7a92c85a3
github-user92 <j_johannsmeier@gmx.de>
2018-08-29 23:35:31 +0200

    added to repo

---
+++ w2l/procedures.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from .estimator_main import run_asr
+
+
+def speaker_averages(data_format="channels_first", *args, **kwargs):
+    """Compute average latent vectors for each speaker.
+
+    Parameters:
+        data_format: channels_first or last.
+        args: Arguments passed to run_asr.
+        kwargs: Keyword arguments passed to run_asr.
+    """
+    time_ax = 1 if data_format == "channels_first" else 0
+    predictions = run_asr("predict", use_ctc=False, *args, **kwargs)
+    store = {}
+    print("Collecting all latent representations...")
+    for ind, pr in enumerate(predictions, start=1):
+        logits = pr["all_layers"][-2][1]
+        latent = pr["all_layers"][-1][1]
+        speaker = pr["speaker"]
+        if speaker not in store:
+            speaker[store] = [1, np.mean(logits, axis=time_ax),
+                              np.mean(latent, axis=time_ax)]
+        else:
+            speaker[store][0] += 1
+            speaker[store][1] += np.mean(logits, axis=time_ax)
+            speaker[store][2] += np.mean(latent, axis=time_ax)
+        if not ind % 500:
+            print("Done with {}...".format(ind))
+    for sp in store:
+        store[sp][1] /= store[sp[0]]
+        store[sp][2] /= store[sp[0]]
+
+    return store
