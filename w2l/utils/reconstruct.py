1fe0436ae4dfb956c4f2a5c623390130e5807854
github-user92 <j_johannsmeier@gmx.de>
2018-10-20 19:20:17 +0200

    removed magic numbers

--- w2l/utils/reconstruct.py
+++ w2l/utils/reconstruct.py
@@ -2,22 +2,24 @@ import librosa
 import numpy as np
 
 
-def reconstruct_from_mag_phase(mag, phase):
+def reconstruct_from_mag_phase(mag, phase, hop_length=160):
     """Reconstruct audio from magnitude and phase.
 
     Parameters:
-        mag: 2D magnitude spectrogram, bins x time.
+        mag: 2D linear (!) magnitude spectrogram, bins x time.
         phase: 2D phase spectrogram bins x time.
+        hop_length: Hop length used to create the spectrograms.
 
     Returns:
         The reconstructed time domain signal as a 1-dim Numpy array.
     """
     rec = mag * np.exp(1.j * phase)
-    rec = librosa.istft(rec, hop_length=160, center=True)
+    rec = librosa.istft(rec, hop_length=hop_length, center=True)
     return rec
 
 
-def griffin_lim(mag, iterations, verbose=False):
+def griffin_lim(mag, iterations, window_size=400, hop_length=160,
+                verbose=False):
     """Reconstruct an audio signal from a magnitude spectrogram.
 
     Given a magnitude spectrogram as input, reconstruct
@@ -27,26 +29,30 @@ def griffin_lim(mag, iterations, verbose=False):
     Processing. Vol ASSP-32, No. 2, April 1984.
 
     Parameters:
-        mag: 2D magnitude spectrogram, bins x time.
+        mag: 2D linear (!) magnitude spectrogram, bins x time.
         iterations: Number of iterations for the Griffin-Lim algorithm.
                     Typically a few hundred is sufficient.
+        window_size: Window size used to create the spectrogram.
+        hop_length: Hop length used to create the spectrogram.
         verbose: Set to print reconstruction error after each iteration.
 
     Returns:
         The reconstructed time domain signal as a 1-dim Numpy array.
     """
     time_slices = mag.shape[1]
-    len_samples = int((time_slices-1)*160)
+    len_samples = int((time_slices-1)*hop_length)
     # Initialize the reconstructed signal to noise
     x_reconstruct = np.random.randn(len_samples)
     for ii in range(iterations):
-        recon_spectro = librosa.stft(x_reconstruct, n_fft=400, hop_length=160)
+        recon_spectro = librosa.stft(x_reconstruct, n_fft=window_size,
+                                     hop_length=hop_length)
         recon_angle = np.angle(recon_spectro)
         # Discard magnitude part of the reconstruction and use the supplied
         # magnitude spectrogram instead
         proposal_spectrogram = mag * np.exp(1.0j * recon_angle)
         prev_x = x_reconstruct
-        x_reconstruct = librosa.istft(proposal_spectrogram, hop_length=160)
+        x_reconstruct = librosa.istft(proposal_spectrogram,
+                                      hop_length=hop_length)
         diff = np.sqrt(np.sum((x_reconstruct - prev_x)**2)/x_reconstruct.size)
         if verbose or not (ii + 1) % 100:
             print('Reconstruction iteration: {}/{} RMSE: {} '.format(
@@ -54,13 +60,15 @@ def griffin_lim(mag, iterations, verbose=False):
     return x_reconstruct
 
 
-def mel_to_linear(mel, log=False, power=False):
+def mel_to_linear(mel, log=False, power=False, sr=16000, window_size=400):
     """Approximately invert a mel spectrogram to linear frequency bins.
 
     Parameters:
         mel: The mel spectrogram.
         log: If true, interpret input as log mel spectrogram.
         power: If true, interpret mag as a power spectrogram.
+        sr: Sampling rate of the audio data.
+        Window size used to create the spectrogram.
 
     Returns:
         Linear frequency spectrogram.
@@ -68,7 +76,8 @@ def mel_to_linear(mel, log=False, power=False):
     if log:
         mel = np.exp(mel)
 
-    mel_basis = librosa.filters.mel(sr=16000, n_fft=400, n_mels=mel.shape[0])
+    mel_basis = librosa.filters.mel(sr=sr, n_fft=window_size,
+                                    n_mels=mel.shape[0])
     linear = np.dot(mel_basis.T, mel)
 
     if power:
