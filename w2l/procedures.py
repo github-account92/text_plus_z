5663bcab2392c598bc162082c2ad88752c401074
github-user92 <j_johannsmeier@gmx.de>
2018-10-20 18:10:30 +0200

    refactored procedures

--- w2l/procedures.py
+++ w2l/procedures.py
@@ -18,9 +18,9 @@ def compute_all_latents(path, data_format="channels_first", *args, **kwargs):
         **kwargs: Keyword arguments passed to run_asr.
 
     Returns:
-        Dictionary mapping speaker ID to a list containing two elements.
-        Element 1 is an n x v array containing all logits for the speaker.
-        Element 2 is an n x d array containing all latents for the speaker.
+        Dictionary mapping speaker ID to a list containing two elements:
+            Element 1 is an n x v array containing all logits for the speaker.
+            Element 2 is an n x d array containing all latents for the speaker.
     """
     if not os.path.isdir(path):
         os.mkdir(path)
@@ -28,8 +28,8 @@ def compute_all_latents(path, data_format="channels_first", *args, **kwargs):
         store = {}
         print("Collecting all latent representations...")
         for ind, pr in enumerate(predictions, start=1):
-            logits = pr["all_layers"][-2][1]
-            latent = pr["all_layers"][-1][1]
+            logits = pr["logits"]
+            latent = pr["latent"]
             speaker = pr["speaker"]
             if speaker not in store:
                 store[speaker] = [[logits], [latent]]
@@ -40,8 +40,12 @@ def compute_all_latents(path, data_format="channels_first", *args, **kwargs):
                 print("Done with {}...".format(ind))
         print("Storing...")
         for sp in store:
-            store[sp][0] = np.concatenate(store[sp][0], axis=1 if data_format == "channels_first" else 0)
-            store[sp][1] = np.concatenate(store[sp][1], axis=1 if data_format == "channels_first" else 0)
+            store[sp][0] = np.concatenate(
+                store[sp][0], axis=1 if data_format == "channels_first" else 0)
+            store[sp][1] = np.concatenate(
+                store[sp][1], axis=1 if data_format == "channels_first" else 0)
+            # we always store channels_last for convenience
+            # this way the different "samples" are in axis 0
             if data_format == "channels_first":
                 store[sp][0] = store[sp][0].transpose()
                 store[sp][1] = store[sp][1].transpose()
@@ -89,48 +93,49 @@ def get_mmds(store, iters=100, batch_size=10000):
         batch_size: How large the batches for each iteration should be.
 
     Returns:
-        Similar dict containing MMD for each speaker as well as overall MMD
-        in an "<ALL>" key.
+        Similar dict containing MMD for each speaker as well as overall MMD in
+        an "<ALL>" key.
     """
     pl_data = tf.placeholder(tf.float32, shape=[None, None])
     pl_prior = tf.placeholder(tf.float32, shape=[None, None])
 
     mmd = compute_mmd(pl_data, pl_prior)
 
+    def mmd_estimator(_samples, _sess):
+        mmd_est = 0
+        for ii in range(iters):
+            batch_inds = np.random.choice(_samples.shape[0],
+                                          size=batch_size, replace=False)
+            latent_batch = latent_samples[batch_inds]
+            gauss_samples = np.random.randn(
+                *latent_batch.shape).astype(np.float32)
+            mmd_est += _sess.run(mmd, feed_dict={pl_data: latent_batch,
+                                                 pl_prior: gauss_samples})
+        return mmd_est / iters
+
     mmd_store = {}
     with tf.Session() as sess:
         for sp in store:
             print("Doing speaker {}...".format(sp))
-            latent_samples = np.concatenate((store[sp][0], store[sp][1]), axis=1)
+            latent_samples = np.concatenate((store[sp][0], store[sp][1]),
+                                            axis=1)
             # normalize latent data so we can compare it to standard Gaussian
-            latent_samples = ((latent_samples - np.mean(latent_samples, axis=0, keepdims=True)) /
-                             np.std(latent_samples, axis=0, keepdims=True))
+            latent_samples = ((latent_samples - np.mean(latent_samples, axis=0,
+                                                        keepdims=True)) /
+                              np.std(latent_samples, axis=0, keepdims=True))
 
-            # to make computation manageable, we repeatedly sample batches from the samples
-            mmd_store[sp] = 0
-            for ii in range(iters):
-                batch_inds = np.random.choice(latent_samples.shape[0], size=batch_size, replace=False)
-                latent_batch = latent_samples[batch_inds]
-                gauss_samples = np.random.randn(*latent_batch.shape).astype(np.float32)
-                mmd_store[sp] += sess.run(mmd,
-                                          feed_dict={pl_data: latent_batch,
-                                                     pl_prior: gauss_samples})
-            mmd_store[sp] /= iters
+            # to make computation manageable, we repeatedly sample batches from
+            # the samples
+            mmd_store[sp] = mmd_estimator(latent_samples, sess)
 
         all_logits = np.concatenate([store[sp][0] for sp in store])
         all_latent = np.concatenate([store[sp][1] for sp in store])
         all_latent_samples = np.concatenate((all_logits, all_latent), axis=1)
-        all_latent_samples = ((all_latent_samples - np.mean(all_latent_samples, axis=0, keepdims=True)) /
+        all_latent_samples = ((all_latent_samples - np.mean(all_latent_samples,
+                                                            axis=0,
+                                                            keepdims=True)) /
                               np.std(all_latent_samples, axis=0, keepdims=True))
 
-        mmd_store["<ALL>"] = 0
-        for ii in range(iters):
-            batch_inds = np.random.choice(all_latent_samples.shape[0], size=batch_size, replace=False)
-            latent_batch = all_latent_samples[batch_inds]
-            gauss_samples = np.random.randn(*latent_batch.shape).astype(np.float32)
-            mmd_store["<ALL>"] += sess.run(mmd,
-                                           feed_dict={pl_data: latent_batch,
-                                                      pl_prior: gauss_samples})
-        mmd_store["<ALL>"] /= iters
+        mmd_store["<ALL>"] = mmd_estimator(all_latent_samples, sess)
 
     return mmd_store
