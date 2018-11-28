072fb79339b7ee72bf968db1e7dfc5c327419cf4
github-user92 <j_johannsmeier@gmx.de>
2018-11-28 10:27:37 +0100

    added image grid creation

--- w2l/utils/viz.py
+++ w2l/utils/viz.py
@@ -156,3 +156,60 @@ def fig_latent(latent, sr=16000, hop_length=320, labelsize=18, ticksize=15,
         plt.savefig(store, bbox_inches="tight")
     if show:
         plt.show()
+
+
+def img_grid_npy(imgs, dims, borders=(1, 1), border_val=None, normalize=True):
+    """
+    Pack a bunch of image-like arrays into a grid for nicer visualization.
+
+    Parameters:
+        imgs: Should be a list of arrays, each height x width. E.g. if you have
+              32 filter maps, i.e. array height x width x 32 you should pack
+              those into a list of 32 elements of size height x width.
+              Alternatively you can just have a 32 x height x with array.
+        dims: Tuple, how many rows/columns to use for the grid. Product has to
+              match the number of images!
+        borders: Size of the borders between images. Tuples; first entry gives
+                 row borders and second entry column borders.
+        border_val: What value to use for the borders inbetween images. If not
+                    given, the maximum over all images will be used.
+        normalize: If True, normalize each image separately to absolute maximum
+                   of 1. Only scaled, not shifted!
+
+    Returns:
+         A 2d tensor you can use for matplotlib.
+    """
+    rows, cols = dims
+    if len(imgs) != rows * cols:
+        raise ValueError("Grid doesn't match the number of images!")
+
+    if normalize:
+        def norm(img):
+            return img / np.abs(img).max()
+
+        imgs = np.array([norm(img) for img in imgs])
+
+    if border_val is None:
+        border_val = imgs.max()
+
+    # make border things
+    col_border = np.full([imgs[0].shape[0], borders[1]], border_val)
+
+    # first create the rows
+    def make_row(ind):
+        base = imgs[ind:(ind + cols)]
+        _borders = [col_border] * len(base)
+        _interleaved = [elem for pair in zip(base, _borders) for
+                        elem in pair][:-1]  # remove last border
+        return _interleaved
+
+    grid_rows = [np.concatenate(make_row(ind), axis=1) for
+                 ind in range(0, len(imgs), cols)]
+
+    # then stack them
+    row_border = np.full([borders[0], grid_rows[0].shape[1]], border_val)
+    borders = [row_border] * len(grid_rows)
+    interleaved = [elem for pair in zip(grid_rows, borders) for
+                   elem in pair][:-1]
+    grid = np.concatenate(interleaved)
+    return grid
